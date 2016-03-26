94d3f86866f4ec90a3554838f89f889ddebb5ba2
Alex Stevenson <alexstevenson19@gmail.com>
2016-03-26 13:03:42 -0500

    stop point for 4_6 pair work day 2, and started 4_4 Solo challenge.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -18,3 +18,82 @@ end
 
 puts password
 # output 
+
+
+
+# Decrypt password
+
+# enter encrpyted password
+
+# set up (backward?) alphabet code corresponding to (numbers?).
+@a, @b, @c, @d, @e, @f = 0, 1, 2, 3, 4, 5
+# or
+"z, y , x, w, "
+
+def codebreaker(password)
+	puts password.split(//)
+
+end
+# break apart password string into separate characters (an array?).
+
+# match the characters to the corresponding letters in code-breaker.
+
+# minus 1 space to get decrypted letter.
+
+# output letter.
+
+
+
+
+
+# -----------------------------------
+
+# alphabetstring = "abcdefghijklmnopqrstuvqxyz"
+# alphabetstring.index("a")
+
+
+# puts ("a".."z").to_a
+ 
+# #=> [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+ 
+# ("a".."z").each{|letter| puts letter}
+
+
+
+# @a, @b, @c, @d, @e, @f = 0, 1, 2, 3, 4, 5
+
+# def alpha_test
+
+#   p @a, @c
+  
+# end
+
+# alpha_test
+
+# puts "Type in a word"
+# word = gets.chomp
+
+# text = word.split("")
+# puts text
+
+alphabetstring = "abcdefghijklmnopqrstuvwxyz".split("")
+p arr = alphabetstring.index("w").to_i
+p arr_1 = arr - 1
+p alphabetstring[arr_1]
+# puts alphabetstring.shift
+
+# rev_alphabetstring = "zyxwvutsrqp0nmlkjihgfedcba".split("")
+
+# p arr = rev_alphabetstring.index("w").to_i
+# p arr_1 = arr - 1
+# p rev_alphabetstring[arr_1]
+
+# def decrypt(password)
+#   index = 0
+#     while index < password.length
+#      password[index] = password[index]
+#        index+=1
+#     end
+# puts password
+# end
+# decrypt("bcd")
---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+#Gather Data
+puts "What's your name?"
+applicant_name = gets.chomp
+
+puts "How old are you?"
+applicant_age = gets.chomp
+
+puts "What year were you born?"
+applicant_dob = gets.chomp
+
+puts "Would you like garlic bread at lunch, yes or no?"
+applicant_garlic = gets.chomp
+
+puts "Would you like to enroll in the health insurance program?"
+applicant_health = gets.chomp
