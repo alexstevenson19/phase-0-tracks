fb78747b1e3631280f674eaaed775f9aeb71373a
Alex Stevenson <alexstevenson19@gmail.com>
2016-03-29 22:02:00 -0500

    do 5_2 throught release01

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,1 +1,38 @@
+array_5_2 = []
 
+first_array = array_5_2
+p first_array
+
+
+array_5_2 << "car"
+
+# print first_array
+
+array_5_2 << "boat"
+array_5_2 << "plane"
+array_5_2 << "bike"
+array_5_2 << "chopper"
+
+p array_5_2
+
+array_5_2.delete_at(2)
+
+p array_5_2
+
+array_5_2.insert(2, "skateboard")
+
+p first_array
+
+array_5_2.delete("car")
+
+p first_array
+
+p "Is a bike included in the array: #{array_5_2.include?("bike")}."
+
+p "Is a car included in the array: #{array_5_2.include?("car")}."
+
+new_array_5_2 = ["Chicago", "Denver", "Des Moines"]
+
+array_mash = array_5_2 + new_array_5_2
+
+p array_mash
