13b5472ee7463d44fdb6d024c9101d13427d80e0
Alex Stevenson <alexstevenson19@gmail.com>
2016-03-30 22:23:05 -0500

    5_2 Release 3 initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+
+# Interior Design interface
+
+# info: client name, age, family/children, decor theme, outside space, favorite 3 colors, etc.
+
+# prompt user to input data
+
+# convert data to appropriate data type
+
+# print report (hash) at end
+
+# allow for updates, and update the proper key
+
+# print updated report and exit
+
+# set up hash
+interior_client = {}
