fe257a5a8d3c35c9277f0b6dedae7a94264ecfd8
Alex Stevenson <alexstevenson19@gmail.com>
2016-03-30 23:32:21 -0500

    progress made in collecting and outputing data for 5_3 release3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -15,3 +15,37 @@
 
 # set up hash
 interior_client = {}
+
+# get info
+puts "Please enter client's first and last name."
+
+interior_client[:fullname] = gets.chomp
+
+puts "Please enter client's age"
+
+interior_client[:age] = gets.chomp.to_i
+
+puts "Please enter the total number of family members residing at residence."
+
+interior_client[:familysize] = gets.chomp.to_i
+
+puts "Are there pets? (y/n)"
+
+pets = gets.chomp.downcase
+
+# :pet_type = []
+# def pet_followup (pets)
+	if pets = "y"
+		"Please enter type of pet(s)."
+		interior_client[:pet_type] = gets.chomp
+	end
+# interior_client[:pet_type]
+
+
+puts "What is the preferred decor style?"
+
+interior_client[:decorstyle] = gets.chomp
+
+
+puts "Interior Decor report\n 
+	Client Name: #{ interior_client[:fullname] }"
