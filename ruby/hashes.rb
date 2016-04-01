c06b6b3a707ad6599ac743d33ff3393522bd4c5c
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-01 17:09:04 -0500

    update section is mostly working after office hours session.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -29,18 +29,18 @@ puts "Please enter the total number of family members residing at residence."
 
 interior_client[:familysize] = gets.chomp.to_i
 
-puts "Are there pets? (y/n)"
+puts "Do you have any pets (yes/no)?"
 
-pets = gets.chomp.downcase
+interior_client[:pets] = gets.chomp == "yes"
 
-	if pets == "y"
-		interior_client[:pet] = true
-		"Please enter type of pet(s)."
-		interior_client[:pet_type] = gets.chomp
-	else
-		pets == "n"
-		interior_client[:pet] = false
-	end
+	# if pets == "y"
+	# 	interior_client[:pet] = true
+	# 	"Please enter type of pet(s)."
+	# 	interior_client[:pet_type] = gets.chomp
+	# else
+	# 	pets == "n"
+	# 	interior_client[:pet] = false
+	# end
 # interior_client[:pet_type]
 
 
@@ -49,7 +49,33 @@ puts "What is the preferred decor style?"
 interior_client[:decorstyle] = gets.chomp
 
 
+
+
 puts "Interior Decor report\n 
 	Client Name: #{ interior_client[:fullname] }\n
 	Family Size: #{ interior_client[:familysize] } person(s) \n
 	"
+
+p interior_client
+
+# get updated information
+done = false
+until done == true
+  puts "Please review and  enter any changes listing the category and updated information. Type 'done' when finished."
+  updates = gets.chomp.to_sym
+
+  if updates == "done"
+    done = true
+  else
+    puts "Ok what would you like like to update at this value?"
+    interior_client[updates] = gets.chomp
+  end
+
+end
+
+p interior_client
+
+# interior_client[:pets] = "dog"
+
+p interior_client
+
