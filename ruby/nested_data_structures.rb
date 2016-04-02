0ce7cfbf997e5ea358f59e921e6f8cb4a4516dc9
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-01 22:42:27 -0500

    initial commit, psudocode of theme (bookshelf-top row) and content in each section

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+
+# Index Top Row of book shelf
+
+# four cubes 1..4 (left to right)
+
+=begin cube 1 contents: reading materials, stock photo books, paperback reference, Stay Free magazine.
+	misc items: clock
+=end
+
+=begin cube 2: reading materials art and film textbooks, graphic design magazines.
+	souvenir beverages - canned coffee Mucha designs, children's beer bottles,
+	misc: mini barrel of monkeys
+=end
+
+=begin cube 3: mounted Stay Free cover Apple ad spoof. inside "magazine holder" [Maps] travel books-(including Cool Spots zine)
+	cook books, photo album
+=end
+
+=begin cube 4: One yellow Ikea box: photos from Japan, two blue binder albums, mini album from Dad.
+=end
