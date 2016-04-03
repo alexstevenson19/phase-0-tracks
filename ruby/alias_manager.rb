de014f9ce711906cf62c5fb3d6eb4bf142094c16
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-02 19:21:03 -0500

    interface works, still need to add loop.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,26 +1,44 @@
 
-#swap first and last name
-switch = "Virgina Hall".split (' ')
-switch.reverse
+spy_alias = {}
 
-#? break the two words into separate variable to work with them?
-# method to tackle the vowels first, then a method for the consonants.
+puts "Hello, Welcome to Alias Maker 2016.\n
+	Please enter the name you would like to encrypt (- When finished type 'quit') "
+
+	#swap first and last name by making the words into two arrays with the method split quote space quote (index 0 and 1) by, and then reversing that array
+	original_name = gets.chomp
+	switch = original_name.split (' ')
+	switch.reverse!
+
+	#turn the array indexes into strings
+	switch_first = switch[0].downcase
+	switch_second = switch[1].downcase
 
 
-switch_first = switch[0].downcase#.split('')
-switch_second = switch[1].downcase#.split('')
+	#not the most elegant way to change the letters, but I was stuck with arrays and hashes. gsub! worked
+	#technically I could use this method for all of the vowels and consonats at once, but it is a little easier to read this way.
 
-switch_first.gsub!(/[aeiou]/, "a"=>"e", "e"=>"i", "i"=>"o", "o"=>"u", "u"=>"a")
-switch_second.gsub!(/[aeiou]/, "a"=>"e", "e"=>"i", "i"=>"o", "o"=>"u", "u"=>"a")
+	switch_first.gsub!(/[aeiou]/, "a"=>"e", "e"=>"i", "i"=>"o", "o"=>"u", "u"=>"a")
+	switch_second.gsub!(/[aeiou]/, "a"=>"e", "e"=>"i", "i"=>"o", "o"=>"u", "u"=>"a")
 
 
-switch_first.gsub!(/[bcdfghjklmnpqrstvwxyz]/, "b"=>"c","c"=>"d","d"=>"f","f"=>"g","g"=>"h","h"=>"j","j"=>"k","k"=>"l","l"=>"m",
-					"m"=>"n","n"=>"p","p"=>"q","q"=>"r","r"=>"s","s"=>"t","t"=>"v","v"=>"w","w"=>"x","x"=>"y","y"=>"z","z"=>"b",)
-					
-switch_second.gsub!(/[bcdfghjklmnpqrstvwxyz]/, "b"=>"c","c"=>"d","d"=>"f","f"=>"g","g"=>"h","h"=>"j","j"=>"k","k"=>"l","l"=>"m",
-					"m"=>"n","n"=>"p","p"=>"q","q"=>"r","r"=>"s","s"=>"t","t"=>"v","v"=>"w","w"=>"x","x"=>"y","y"=>"z","z"=>"b",)
+	switch_first.gsub!(/[bcdfghjklmnpqrstvwxyz]/, "b"=>"c","c"=>"d","d"=>"f","f"=>"g","g"=>"h","h"=>"j","j"=>"k","k"=>"l","l"=>"m",
+						"m"=>"n","n"=>"p","p"=>"q","q"=>"r","r"=>"s","s"=>"t","t"=>"v","v"=>"w","w"=>"x","x"=>"y","y"=>"z","z"=>"b",)
+						
+	switch_second.gsub!(/[bcdfghjklmnpqrstvwxyz]/, "b"=>"c","c"=>"d","d"=>"f","f"=>"g","g"=>"h","h"=>"j","j"=>"k","k"=>"l","l"=>"m",
+						"m"=>"n","n"=>"p","p"=>"q","q"=>"r","r"=>"s","s"=>"t","t"=>"v","v"=>"w","w"=>"x","x"=>"y","y"=>"z","z"=>"b",)
 
-puts "#{switch_first} #{ switch_second}"
+	#capitalize the names
+	switch_first.capitalize!
+	switch_second.capitalize!
+
+alias_name = "#{switch_first} #{ switch_second}"
+
+spy_alias[original_name] = alias_name
+
+# p spy_alias
+spy_alias.each do |original_name, alias_name| 
+	puts "#{alias_name} is #{original_name}"
+end
 
 
 =begin
