1d70af60e5a7beede6b1eb4fffa39b8fa136cbb1
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-04 21:04:02 -0500

    initial commit for GPS 2_2, and accidentally started on 6_1 puppy file on this branch

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+
+=begin
+	SPECIES - Canine
+	Characteristics -
+		surface: fluffy
+		eyes: warm
+		personality: loyal, funny
+
+	Behavior -
+		excited when you come home, go to the park, sees a tennis ball
+		picky about territory
+=end
