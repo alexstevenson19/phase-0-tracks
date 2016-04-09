1614c9767357b176cfd21433d671f46dfc4c9cad
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-09 14:04:46 -0500

    latest saves for list rspec exercise, but it is not working at this point

--- ruby/list/list.rb
+++ ruby/list/list.rb
@@ -1,22 +1,65 @@
 
 class TodoList
-  def initialize(chores)
-    @chores = chores  
+  # def initialize
+    # @all_chores = []
+  # end
+  # def all_chores
+    # @all_chores
+  # end
+  def initialize(chores1, chores2)
+  	@all_chores = []
+    @all_chores << chores1
+    @all_chores << chores2
+    # all_chores = @all_chores
+    @all_chores
   end
-
-
-
-  	# x = [chore1, chore2]
-    # @chore1 = chore1
-    # @chore2 = chore2
-
-
+    
 end
 
+list = TodoList.new("do the dishes", "mow the lawn")
+# list.get_items("do the dishes", "mow the lawn")
 
-chores = TodoList.new
-p chores
 
 
-list = TodoList.new(["do the dishes", "mow the lawn"])
-p list
+
+
+
+
+# class TodoList
+# 
+  # def initialize
+# 
+  # end
+# 
+  # def get_items ([chores1, chores2])
+  	# chore_array = [chores1, chores2]
+	# chore_array << chores
+# 
+  	# def get_items(chore1, chore2)
+  		# @chore_array = ["do the dishes", "mow the lawn"]
+  		
+  	# end
+  	# chore_array << ["do the dishes", "mow the lawn"]
+  	# @chore_array = chore_array
+    # 
+  # end
+# 
+# 
+  # def add_item(new_chore)
+  	# @chore_array << "mop"
+# 
+  # end
+# 
+# 
+# end
+# 
+# x = "do the dishes, mow the lawn"
+# 
+# chores_test = TodoList.new
+# chores_test.get_items("do the dishes", "mow the lawn")
+# 
+
+#  chore_test.get_items()
+
+# list = TodoList.new(["do the dishes", "mow the lawn"])
+# p list
