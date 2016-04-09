63ec0f7c25f01475088c925d001e145ce105e457
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-08 20:51:58 -0500

    initial commit for 6_6 pair

---
+++ ruby/list/list.rb
@@ -0,0 +1,22 @@
+
+class TodoList
+  def initialize(chores)
+    @chores = chores  
+  end
+
+
+
+  	# x = [chore1, chore2]
+    # @chore1 = chore1
+    # @chore2 = chore2
+
+
+end
+
+
+chores = TodoList.new
+p chores
+
+
+list = TodoList.new(["do the dishes", "mow the lawn"])
+p list
