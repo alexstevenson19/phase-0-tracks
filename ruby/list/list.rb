57c5aeec67497fbb8fb5b1ab30c60b15b59fbd00
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-09 22:48:56 -0500

    complete list.rb file for rspec test.

--- ruby/list/list.rb
+++ ruby/list/list.rb
@@ -1,65 +1,30 @@
 
+
 class TodoList
-  # def initialize
-    # @all_chores = []
-  # end
-  # def all_chores
-    # @all_chores
-  # end
-  def initialize(chores1, chores2)
-  	@all_chores = []
-    @all_chores << chores1
-    @all_chores << chores2
-    # all_chores = @all_chores
-    @all_chores
-  end
-    
+ def initialize(all_chores)
+   @all_chores = all_chores
+ end
+ def all_chores
+   @all_chores
+ end
+ def get_items
+   all_chores
+ end
+ 
+ def add_item(thing)
+    all_chores << (thing)   
+ end
+
+ def delete_item(thing)
+    all_chores.delete(thing)
+ end
+
+ def get_item(i)
+    all_chores[i]
+ end
+
 end
 
-list = TodoList.new("do the dishes", "mow the lawn")
-# list.get_items("do the dishes", "mow the lawn")
 
 
 
-
-
-
-
-# class TodoList
-# 
-  # def initialize
-# 
-  # end
-# 
-  # def get_items ([chores1, chores2])
-  	# chore_array = [chores1, chores2]
-	# chore_array << chores
-# 
-  	# def get_items(chore1, chore2)
-  		# @chore_array = ["do the dishes", "mow the lawn"]
-  		
-  	# end
-  	# chore_array << ["do the dishes", "mow the lawn"]
-  	# @chore_array = chore_array
-    # 
-  # end
-# 
-# 
-  # def add_item(new_chore)
-  	# @chore_array << "mop"
-# 
-  # end
-# 
-# 
-# end
-# 
-# x = "do the dishes, mow the lawn"
-# 
-# chores_test = TodoList.new
-# chores_test.get_items("do the dishes", "mow the lawn")
-# 
-
-#  chore_test.get_items()
-
-# list = TodoList.new(["do the dishes", "mow the lawn"])
-# p list
