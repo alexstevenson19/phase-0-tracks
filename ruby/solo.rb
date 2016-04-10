d3fccf730f3861d4393f870c22cccdf7094a2ed8
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-10 00:08:06 -0500

    initial commit for 6_4 solo challenge.

---
+++ ruby/solo.rb
@@ -0,0 +1,29 @@
+
+# snowboard parks
+
+# attributes: run lengths(long, short), lift music, number of lifts, speciality, snow quality (base)
+	# 
+
+# methods: run color (argument), travel time, travel method (argument?), snowstorm, wrong_turn - you're stuck on an icy black run.
+
+class Snowboard_Park
+
+	attr_reader :lift_music
+	attr_accessor :name
+
+	def initialize(name)
+		puts "Let's go snowboarding at #{name}"
+	end
+
+
+
+
+end
+
+
+
+
+# user assigns a class to a string - snowboard park name.
+
+
+# a user calls a method such as Zao.snowstorm, puts "Yay, fresh powder"
