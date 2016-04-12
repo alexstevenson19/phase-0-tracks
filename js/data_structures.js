81a09b795b595d15d3c91fc53c4b8a28c22c5c32
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-12 17:20:16 -0500

    initial commit with release 0

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+
+var colors = ["blue", "red", "green", "orange"];
+  
+var horseNames = ["Tony", "Brevit", "Bojack", "Ed"];
+
+colors.push("yellow")
+horseNames.push("Joe")
+
+console.log(colors, horseNames)
