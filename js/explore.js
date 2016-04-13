bd1116bc0cd4f7e362111836a0406af2a4e57a6c
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-12 23:19:26 -0500

    reverser works with driver code in a function

--- js/explore.js
+++ js/explore.js
@@ -1,10 +1,23 @@
 
 // assign a word to a variable
 
-var str = "hello";
+//var str = "hello";
 
 // put the variable through a loop, but count down from the end to the beginning
+// put the loop in a function so you can use it with driver code
 
-for (var i = str.length; i >= 0; i-=1) {
+//conditionTest = true;
+
+//if (1==1) {
+
+function reverser(str) {
+
+for (var i = str.length; i >= 0; i--) {
 	console.log(str[i]);
+};
 }
+// else {console.log("The reverser is out of order today.")}
+//}
+
+// driver code
+reverser("United States")
