a8d28379fa3b18fcc690acc326b6ea08d83eff0b
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-13 23:12:54 -0500

    complete including release 8

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -19,50 +19,79 @@ class VirusPredictor
   end
 # Method that calls other methods
   def virus_effects
-    predicted_deaths#(@population_density)   #@state  , @population
-    speed_of_spread#(@population_density)   # @state
+    predicted_deaths     #(@population_density)   #@state  , @population
+    speed_of_spread      #(@population_density)   # @state
   end
 # all methods that follow 'private' will not be accessible for outside objects
   private
 # running if/elsif statements adapting formula and outputting a report
   def predicted_deaths   #(population_density, population)
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
+    
+    
+    case 
+      when @population_density >= 200
+         then number_of_deaths = (@population * 0.4).floor
+      when @population_density >= 150
+         then number_of_deaths = (@population * 0.3).floor
+      when @population_density >= 100
+         then number_of_deaths = (@population * 0.2).floor  
+      when @population_density >= 50
+         then number_of_deaths = (@population * 0.1).floor
+      when @population_density >= 0
+         then number_of_deaths = (@population * 0.05).floor
+ 
     end
+    
+    
+    
+#     
+    # if @population_density >= 200
+      # number_of_deaths = (@population * 0.4).floor
+    # elsif @population_density >= 150
+      # number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+      # number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+      # number_of_deaths = (@population * 0.1).floor
+    # else
+      # number_of_deaths = (@population * 0.05).floor
+    # end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 # running if/eslif statements adapting formula and outputting a report
-  def speed_of_spread #in months   (population_density, state)
+   def speed_of_spread #in months   (population_density, state)
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
+    
+    case 
+      when @population_density >= 200 then speed += 0.5
+      when @population_density >= 150 then speed += 1
+      when @population_density >= 100 then speed += 1.5
+      when @population_density >= 50 then speed += 2
+      when @population_density >= 0 then speed += 2.5  
+        
+   end
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+    # if @population_density >= 200
+      # speed += 0.5
+    # elsif @population_density >= 150
+      # speed += 1
+    # elsif @population_density >= 100
+      # speed += 1.5
+    # elsif @population_density >= 50
+      # speed += 2
+    # else
+      # speed += 2.5
+    # end
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
   end
+
 
 end
 
