8d2d18869a4f2343dc1daa3a6bf8c86c7c4b9e75
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-24 14:40:00 -0500

    initial commit for alias manager

---
+++ ruby/alias_manager_nh.rb
@@ -0,0 +1,9 @@
+
+=begin 
+	- split the string into an array
+	- reverse the array to make the first and last names switch
+
+	- put the word in a method to change the vowels and consonants. Of course I could do this with gsub, but will try an alternative method.
+	- when the method is called and argument sent in
+		1. downcase the string
+=end
