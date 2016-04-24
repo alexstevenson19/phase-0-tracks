8e241900e411778990e66dc99b43319912495cce
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-24 12:47:05 -0500

    initial commit, set up nested arrays and hashes

---
+++ ruby/nested_data_structures_nh.rb
@@ -0,0 +1,21 @@
+
+# Build a list of some favorite comic book series.
+# The overall comic structure will be a hash. Inside the keys will be writers, publishers, or styles.
+# lists of comics by writers will be in arrays
+# list of indie books will be in a hash, with writers as keys
+
+
+comics:{
+	grant_morrison:['Doom Patrol', 'Invisibles', 'Flex Mentallo', 'The Filth'],
+
+	greg_rucka:['Whiteout', 'Gotham Central', 'Lazarus'],
+
+	ed_brubaker:{criminal_series:['Coward', 'Lawless', 'Dead and the Dying', 'Bad_Night',
+									'The Sinners', 'Last of the Innocent']
+				fatale:['Death Chases Me', 'Devils Business', 'West of Hell', 'Pray For Rain'
+						'Curse the Demon'],
+				brubaker_misc:['Scene of the Crime', 'Incongito', 'Fade Out']
+	}
+	indie:{evan_dorkin:'Hectic Planet', jhonen_vasquez: 'Squee', matt_kindt: 'mind mgmt'}
+
+}
