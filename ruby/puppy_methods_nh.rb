24d0cb61474b794e97a22a815e80486b9110efcc
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-26 20:17:29 -0500

    6_2 pair work complete through release2

--- ruby/puppy_methods_nh.rb
+++ ruby/puppy_methods_nh.rb
@@ -1,4 +1,8 @@
 
+# Release 0 and 1 =========================================
+
+# initialize a Puppy class
+# add methods to the class and call them in driver code
 
 class Puppy
 
@@ -8,7 +12,7 @@ class Puppy
   end
   
   def speak(x)
- 	x.times {puts "Woof"}
+  x.times {puts "Woof"}
   end
   
   def roll_over
@@ -17,18 +21,17 @@ class Puppy
   
   def dog_years(h)
     if h.to_i < 3
-     	d = h.to_i * 10
-     	p d
-     else 
-     	d = h.to_i * 4
-     	p d
+      d = h.to_i * 10
+     p d
+      else 
+      d = h.to_i * 4
+      p d
     end
       
   end
-
-  def shake(y) 
-    y.times {puts "Shake paw!"}
-  end
+     def shake(y) 
+       y.times {puts "Shake paw!"}
+     end
   
   def initialize
     puts "Initializing new puppy instance..."
@@ -42,3 +45,89 @@ fido.speak(3)
 fido.roll_over 
 fido.dog_years(2)
 fido.shake(5)
+
+
+
+# Release 2===============================================
+# initialize a class of our choosing
+# create 50 instances of the class and store them in an array
+# iterate through the array and call the methods for the class.
+
+
+class Cats
+  
+  def initialize
+    puts "One of the litter is being created..."
+  end
+  
+  def name(count)
+    puts "Cat #{count}"
+  end
+  
+  def sound(x)
+    (x).times {puts "purrrr"}
+  end
+  
+  def destruction
+    puts "I ruined the sofa."
+  end
+  
+  def hunting
+    puts "Brings you a fresh mouse."
+  end
+  
+end
+
+
+
+cat_litter = []
+
+counter = 0
+
+while counter < 50
+  cat = Cats.new
+  cat_litter.push(cat)
+  
+  counter += 1
+  
+end
+
+
+
+cat_counter = 1
+
+cat_litter.each do |cat|
+                cat.name(cat_counter)
+                cat.sound(2)
+                cat.destruction
+                cat.hunting
+                cat_counter += 1
+              end
+
+
+# end of release 2=========================================
+
+
+
+
+
+
+# extra method experiement for my record. =================
+
+# toy = []
+# 
+  # def fetch(toy)
+    # puts "I changed the toy into a stick.!"
+    # toy.push("stick")
+    # toy
+  # end
+# 
+# 
+# 
+# toy.push("bone")
+# 
+# p toy
+# 
+# fetch(toy)
+# 
+# p toy
