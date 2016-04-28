46822dd83a172463765bdfffdd36fbf3e0c2cc55
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-27 20:04:43 -0500

    initial commit for santa_nh file

---
+++ santa_nh.rb
@@ -0,0 +1,17 @@
+
+# make a Santa class
+
+
+class Santa
+
+	def initialize
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaapy Holidays!"
+	end
+
+	def cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+end
