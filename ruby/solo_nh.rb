012afbf9ed9404acfeafaada4ec58a3f24cdcd5b
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-29 00:09:19 -0500

    blackjack dealer class built with two game methods - honest and bottom of the deck

--- ruby/solo_nh.rb
+++ ruby/solo_nh.rb
@@ -5,8 +5,8 @@ attributes: species: suggestions: human, martian, octopus, robot, Vulcan
 					 number of dealing arms/tentacles, 
 					 bowtie color
 
-methods: honest - 3 random cards (1..13), 
-		 bottom_of_deck, 3 random cards (5..13)
+methods: honest - 4 random cards (1..11), 
+		 bottom_of_deck, 3 random cards (5..11)
 		 kindly_grandmother - 3 random cards (1..8), 
 		 Karnak - two cards ace and king - instant win! 
 
@@ -20,7 +20,7 @@ class Black_jack_dealer
 	attr_reader	:bowtie
 
 	def initialize(species, appendages)
-		puts "Shuffling the deck."
+		puts "Shuffling the deck..."
 		@species = species
 		@appendages = appendages.to_i
 		@bowtie = "red & black polka dots"
@@ -32,9 +32,10 @@ class Black_jack_dealer
 		@card1 = rand(1..11)
 		@card2 = rand(1..11)
 		@card3 = rand(1..11)
-		puts "Here are your cards: #{@card1}, #{@card2}, #{@card3}"
+		@card4 = rand(1..10)
+		puts "Here are your cards: #{@card1}, #{@card2}, #{@card3}, #{@card4}"
 
-		honest_hand = @card1 + @card2 + @card3
+		honest_hand = @card1 + @card2 + @card3 + @card4
 			if honest_hand <= 21
 				puts "You won!"
 				true
@@ -43,5 +44,34 @@ class Black_jack_dealer
 				false
 			end
 	end
+
+	def bottom_of_deck
+		@card1 = rand(5..11)
+		@card2 = rand(5..11)
+		@card3 = rand(5..11)
+		puts "Here are your cards: #{@card1}, #{@card2}, #{@card3}"
+
+		bottom_deck = @card1 + @card2 + @card3
+			if bottom_deck <= 21
+				puts "You won! It's really your lucky day."
+				true
+			else
+				puts "I'm afraid I'm gonna need a bit more of your pinky finger between these here cutters..."
+				false
+			end
+	end
+
+
 end
+
+
+dave = Black_jack_dealer.new("Martian", 4)
+
+p dave.species
+p dave.appendages
+
+dave.honest
+dave.bottom_of_deck
+
+
 
