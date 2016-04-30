d0279b5051bb40baf16a1e41761386f9f796cda8
Alex Stevenson <alexstevenson19@gmail.com>
2016-04-30 15:11:44 -0500

    modify program to create multiple instances, and thus play multiple games at different blackjack tables

--- ruby/solo_nh.rb
+++ ruby/solo_nh.rb
@@ -12,7 +12,7 @@ methods: honest - 4 random cards (1..11),
 
 interface - ask the player to play or cash out. If play, pick random number for case statement.
 
-final report - iterate through the cashout hash and make a report.
+final report - select the number of wins in the cashout hash and make a report.
 =end
 
 
@@ -24,10 +24,9 @@ class Blackjack_dealer
 	#attr_reader	
 
 	def initialize(species, appendages)
-		puts "Shuffling the deck..."
+		puts "Shuffling the deck with #{appendages} appendages..."
 		@species = species
 		@appendages = appendages.to_i
-		#@cash_out = {}
 	end
 
 	def bowtie(color)
@@ -89,72 +88,84 @@ class Blackjack_dealer
 		true
 	end
 
-	def game_report(cashout)
-		win_record = cashout.select{|game, wins| wins == true}
-		puts "You played #{cashout.length} times and won #{win_record.length} time(s)."
+	def game_report(gamecount)
+		win_record = gamecount.select{|game, wins| wins == true}
+		puts "You played #{gamecount.length} times and won #{win_record.length} time(s) at this table."
 	end
 
 end
 
-
-
-# Make a dealer ===============================================================
+#Enter casino========================================================================
+cashout = false
 
 puts "Welcome to the Intergalactic Orion's Belt Casino. Come win some credits to get off this rock!"
 puts "**********"
-puts "Pick a blackjack table. What species is the dealer? (ex. human, martian, octopus, robot, Vulcan...)"
-species = gets.chomp
-puts "How many dealing arms/appendages/tentacles does the dealer have?"
-appendages = gets.chomp.to_i
-
-dealer = Blackjack_dealer.new(species, appendages)
-
-puts "By the way, what color or pattern is the dealer's bowtie?"
-bow_color = gets.chomp
-
-dealer.bowtie(bow_color)
 
 
-# play blackjack ============================================================
+until cashout
+	puts "Type 'play' to go to a blackjack table or type 'exit' to cash out."
+	gamble = gets.chomp
 
-cash_out = {}
-
-valid_input = false
-game_counter = 0
-
-until valid_input	
-
-	puts "Play or cash out? Please type 'p' to play or 'q' to quit."
-	table = gets.chomp
-	puts "----------------"
-	
-	if table.downcase == "q"
-		valid_input = true
+	if gamble.downcase == "exit"
+		cashout = true
 	else
-		play = rand(1..10)
-		case 
-			when play >=8 && play <=10
-				then result = dealer.honest
-			when play >=5 && play <=7
-				then result = dealer.bottom_of_deck
-			when play >=2 && play <=4
-				then result = dealer.soft_spot
-			when play == 1
-				then result = dealer.carnac	
-		end	
+		# Make a space dealer ===========================================================	
+		puts "**********"
+		puts "Pick a blackjack table. What species is the dealer? (ex. human, martian, octopus, robot, Vulcan...)"
+		species = gets.chomp
+		puts "How many dealing arms/appendages/tentacles does the dealer have?"
+		appendages = gets.chomp.to_i
+		
+		dealer = Blackjack_dealer.new(species, appendages)
+		
+		puts "By the way, what color or pattern is the dealer's bowtie?"
+		bow_color = gets.chomp
+		
+		dealer.bowtie(bow_color)
+		
+		
+		# play blackjack ================================================================
+		
+		games = {}
+		
+		valid_input = false
+		game_counter = 0
+		
+		until valid_input	
+		
+			puts "Play or leave table? Please type 'p' to play or 'q' to quit."
+			deal = gets.chomp
+			puts "----------------"
+			
+			if deal.downcase == "q"
+				valid_input = true
+			else
+				play = rand(1..10)
+				case 
+					when play >=8 && play <=10
+						then result = dealer.honest
+					when play >=5 && play <=7
+						then result = dealer.bottom_of_deck
+					when play >=2 && play <=4
+						then result = dealer.soft_spot
+					when play == 1
+						then result = dealer.carnac	
+				end	
+			end
+			game_counter += 1
+			games[game_counter] = result
+		
+		end
+		
+	games.delete(game_counter) #deleting the last key that was made when the loop ended on quit.
+	dealer.game_report(games)
+
+
 	end
-	game_counter += 1
-	p game_counter
-	cash_out[game_counter] = result
+
+
 
 end
-
-
-cash_out.delete(game_counter) #deleting the last key that was made when the loop ended on quit.
-p cash_out
-
-dealer.game_report(cash_out)
-
 
 
 
