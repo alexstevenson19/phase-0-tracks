ed87a52b73ae46ee97f626e76df5cf79364576ea
Alex Stevenson <alexstevenson19@gmail.com>
2016-05-04 22:52:00 -0500

    better function for reversing a string is written, as well as two steps to print the reversed string on one line.

--- js/explore_nh.js
+++ js/explore_nh.js
@@ -1,16 +1,16 @@
 
 // write a function that reverses a string. ex function('hello') returns olleh
 
-// I know that we can count down.
+// I know that we can count down. I can use the index number from the end to go backwards is one solution.
+// Since the charactes are indexed I will countdown from the highest index number to the lowest to go in reverse order.
+// To print on one line I will put my iterated letters in a new array so they are in reverse order as they are being called.
+// It took a little research to find a .join('') function in Javascript to stitch the string back together on one line.
 
-
-var str = "Hello There!";
-var str2 = "Ohayo Gozaimasu."
 
 var rev = [];
 
-
-function reverse(word){
+// first solution that is a bit clunky
+function reverse1(word){
 	down = (word.length);
   for (var i = 0; i <= word.length; i++) {
   	  console.log(word[down]);
@@ -18,4 +18,22 @@ function reverse(word){
   }
 }
 
-reverse(str2);
+
+
+// second solution that is better and I will add the conditionals to.
+function reverse2(word){
+	for (var i = word.length; i >= 0; i--) {
+		rev.push(word[i]);
+	}
+	console.log(rev.join(''));
+}
+
+
+// Driver Code======================================================
+
+var str1 = "Hello There!";
+var str2 = "Ohayo Gozaimasu."
+
+
+reverse1(str1);
+reverse2(str2);
