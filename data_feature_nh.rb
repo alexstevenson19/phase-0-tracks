e9e6b9ba3a60fa49b4b676f476047596019b9515
Alex Stevenson <alexstevenson19@gmail.com>
2016-05-05 18:35:37 -0500

    initial commit for pair project nh

---
+++ data_feature_nh.rb
@@ -0,0 +1,12 @@
+
+//RELEASE 0: CREATE AN ARRAY
+var horseName = ["Ed", "Mitts", "Beauty", "Sea"];
+var horseColor = ["black", "white", "brown", "pink"];
+console.log(horseName);
+console.log(horseColor);
+
+horseName.push("Biscut");
+horseColor.push("silver");
+
+console.log(horseName);
+console.log(horseColor);
