d101a654d5ccc09982ac2cd59b454b13c8b0c54a
Alex Stevenson <alexstevenson19@gmail.com>
2016-05-04 21:11:08 -0500

    initial commit for 7_1 explore program

---
+++ js/explore_nh.js
@@ -0,0 +1,20 @@
+
+// write a function that reverses a string. ex function('hello') returns olleh
+
+// I know that we can count down.
+
+
+var str = "Hello There!";
+var str2 = "Ohayo Gozaimasu."
+
+var rev = [];
+
+
+
+function reverse(word){
+  for (var i = 0; i < word.length; i++) {
+  	  console.log(word[i]);
+  }
+}
+
+reverse(str2);
