332e46a12968676a1bf6bd1a3c9ce1fc05c8a38b
Alex Stevenson <alexstevenson19@gmail.com>
2016-05-14 19:11:02 -0500

    initial commit - pseudocode basics written

---
+++ databases/sound_files/sound_files.rb
@@ -0,0 +1,17 @@
+
+=begin
+	Make a Olympus Sound File organizer.
+	I have digital recordings of my travels in Japan, but due to the nature of the voice recorder file system, files
+	from similar time frames are broken up between five folders. While I have labeled the files themselves, I still have to 
+	search through the folders to find them.
+
+	Make a Ruby interface to store file information in a data base. 
+	Table categories: theme, when, location, keywords, file folder
+	Have a set number of themes to choose from.
+	When: year and (month(s) or season)
+	Location: an wide variable going from country down to district/neighborhood. ex. Yamagata, or Shimokitazawa, or Golden Gai
+	Keywords: also very wide open.
+	There are only five file folders listed A to E, so they this information can be kept in a small database by itself, and will be a one to five relationship with the main database.
+
+	
+=end
