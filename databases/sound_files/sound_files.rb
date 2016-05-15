c3e7cc210fa35bc761c07eadc3f936f6cc1eecff
Alex Stevenson <alexstevenson19@gmail.com>
2016-05-15 18:14:39 -0500

    working sound file organizer, adding and retrieving works.

--- databases/sound_files/sound_files.rb
+++ databases/sound_files/sound_files.rb
@@ -22,29 +22,18 @@ require 'sqlite3'
 db = SQLite3::Database.new("sound_files.db")
 #db.results_as_hash = true
 
-# I may have to break the INSERT INTO command into a separate .execute command/method, but will try it all together at first.
-# create_theme_table = <<-SQL
-#   CREATE TABLE IF NOT EXISTS themes(
-#   	id INTEGER PRIMARY KEY,
-#   	name VARCHAR(255)
-#   	);
-# SQL
 
-# create_folder_table = <<-SQL
-#   CREATE TABLE IF NOT EXISTS folders(
-#   	id INTEGER PRIMARY KEY,
-#   	name VARCHAR(255)
-#   	);
-# SQL
 
 create_sound_table = <<-SQL
   CREATE TABLE IF NOT EXISTS sound_table (
     id INTEGER PRIMARY KEY,
     theme_id INT,
-	year_when VARCHAR(255),
+	year_when INT,
 	location VARCHAR(255),
 	keywords VARCHAR(255),
-	folder_id INT
+	folder_id INT,
+	FOREIGN KEY (theme_id) REFERENCES themes(id), 
+	FOREIGN KEY (folder_id) REFERENCES folders(id)
 	);
 
   CREATE TABLE IF NOT EXISTS folders(
@@ -60,11 +49,10 @@ create_sound_table = <<-SQL
 SQL
 
 
-# methods for foreign tables with finite information
+# create foreign tables with finite information
 create_folders = <<-SQL
   INSERT INTO folders (name) VALUES ('A'), ('B'), ('C'), ('D'), ('E')
 SQL
-
 
 create_themes = <<-SQL
    INSERT INTO themes (name) VALUES ('Inner Thoughts'), ('Stories'), ('Travel Journal'), ('Japan Life Journal'), 
@@ -74,7 +62,6 @@ create_themes = <<-SQL
 
 
 
-# Need two methods to convert folders and themes into id numbers
 
 def create_soundtable(db, theme, time, location, keywords, folder)
   db.execute("INSERT INTO sound_table (theme_id, year_when, location, keywords, folder_id) 
@@ -82,7 +69,7 @@ def create_soundtable(db, theme, time, location, keywords, folder)
 end
 
 
-
+# Need two methods to convert folders and themes into id numbers
 def soundtable_input_folder
   valid_input = false
   until valid_input
@@ -132,14 +119,14 @@ def soundtable_input_theme
 end
 
 def sound_input_year
-	puts "Please enter the year, and month or season."
-	year_when = gets.chomp
+	puts "Please enter the year."
+	year_when = gets.chomp.to_i
 	year_when
 end
 
 def sound_input_location
 	puts "Please enter the location."
-	location = gets.chomp
+	location = gets.chomp.downcase
 	location
 end
 
@@ -149,9 +136,82 @@ def sound_input_keywords
 	keywords
 end
 
+def sound_input_retrieve(db)
+  output = nil
+  valid_input = false
+  until valid_input
+	puts "How would you like to retrieve information? \n
+  		theme(1), time(2), location(3), keywords(4), all(5)\n
+  		Just type the corresponding number:"
+  		selection = gets.chomp
+  	  if selection.to_i > 5 || selection.to_i < 1
+      		puts "Sorry that is not a selection, please enter the number again."
+      		valid_input = false
 
+      	elsif selection.to_i == 1
+      	  valid_theme = false
+      	  until valid_theme
+    	    puts "Please select a theme. \n
+    	  	Inner Thoughts(1), Stories(2), Travel Journal(3), Japan Life Journal(4), 
+    	      Memo(5), Work Related(6), Brainstorm(7)\n		
+    	      Just type the corresponding number:"
+    	      theme = gets.chomp
+    	        if theme.to_i > 7 || theme.to_i < 1
+    	            puts "Sorry that is not a selection, please enter the number again."
+    	        	  valid_theme = false
+    	        	else
+    	        	  theme
+    	        	  valid_theme = true      		
+    	        end
+    	  end
 
+      		output = db.execute("SELECT themes.name, year_when, location, keywords, folders.name 
+      			FROM sound_table JOIN themes ON sound_table.theme_id = themes.id, 
+      			folders ON sound_table.folder_id = folders.id WHERE theme_id = #{theme}")
+      		valid_input = true
 
+      	elsif selection.to_i == 2
+      	  puts "Please enter the year:"
+      	  year = gets.chomp
+      	  output = db.execute("SELECT themes.name, year_when, location, keywords, folders.name 
+      			FROM sound_table JOIN themes ON sound_table.theme_id = themes.id, 
+      			folders ON sound_table.folder_id = folders.id WHERE year_when = #{year}")
+      	  valid_input = true
+
+      	elsif selection.to_i == 3
+      	  puts "Please enter the location:"
+      	  location = gets.chomp.downcase
+          output = db.execute("SELECT themes.name, year_when, location, keywords, folders.name 
+      			FROM sound_table JOIN themes ON sound_table.theme_id = themes.id, 
+      			folders ON sound_table.folder_id = folders.id WHERE location = '#{location}'")
+      	  valid_input = true
+
+      	elsif selection.to_i == 4
+      	  puts "Please enter the keyword(s):"
+      	  keyword = gets.chomp
+      	  key_check = db.execute("SELECT * FROM sound_table WHERE keywords='#{keyword}'")
+      	    if key_check.length < 1
+      	    	puts "Sorry, no keyword match."
+      	    else
+      	    	output = db.execute("SELECT themes.name, year_when, location, keywords, folders.name 
+      			FROM sound_table JOIN themes ON sound_table.theme_id = themes.id, 
+      			folders ON sound_table.folder_id = folders.id WHERE keywords = '#{keyword}'")
+      			valid_input = true
+      		end
+
+      	else #selection_to_i == 5
+      	  output = db.execute("SELECT themes.name, year_when, location, keywords, folders.name 
+      			FROM sound_table JOIN themes ON sound_table.theme_id = themes.id, 
+      			folders ON sound_table.folder_id = folders.id")
+      	  	valid_input = true
+
+      end #valid input if loop
+      		
+
+  end
+  puts "#{output}"
+  output
+end
 
 
 
@@ -188,20 +248,22 @@ until quit
   selection = gets.chomp
 
   if selection.downcase == "q"
-      	quit = true
+    quit = true
     
   elsif selection.downcase == "add"
-  	  folder = soundtable_input_folder.to_i
-	  theme = soundtable_input_theme.to_i 
-	  time = sound_input_year
-	  location = sound_input_location
-	  keywords = sound_input_keywords
-	  #puts "#{theme}, #{folder}, #{time}, #{location}, #{keywords} "
-
-	  create_soundtable(db, theme, time, location, keywords, folder)
+  	folder = soundtable_input_folder.to_i
+	theme = soundtable_input_theme.to_i 
+	time = sound_input_year
+	location = sound_input_location
+	keywords = sound_input_keywords
+	#puts "#{theme}, #{folder}, #{time}, #{location}, #{keywords} 
+	create_soundtable(db, theme, time, location, keywords, folder)
 
   elsif selection.downcase == "retrieve"
   	#retrieval process
+  	sound_input_retrieve(db)
+  else
+
   end
 
 end
@@ -211,4 +273,22 @@ puts "Thank you for using the Sound File Organizer today."
 
 
 
+
+
+
+#===============================================
+# I may have to break the INSERT INTO command into a separate .execute command/method, but will try it all together at first.
+# create_theme_table = <<-SQL
+#   CREATE TABLE IF NOT EXISTS themes(
+#   	id INTEGER PRIMARY KEY,
+#   	name VARCHAR(255)
+#   	);
+# SQL
+
+# create_folder_table = <<-SQL
+#   CREATE TABLE IF NOT EXISTS folders(
+#   	id INTEGER PRIMARY KEY,
+#   	name VARCHAR(255)
+#   	);
+# SQL
 
